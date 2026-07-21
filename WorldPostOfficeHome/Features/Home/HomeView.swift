6c1f91f999ee4e3e784382a650cc5de97b220d6d
eiranotes <eiradev000@gmail.com>
2026-07-22 08:02:33 +0900

    feat: compose SwiftUI home screen

---
+++ WorldPostOfficeHome/Features/Home/HomeView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            AppColor.background
+                .ignoresSafeArea()
+
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 16) {
+                    UserHeaderView()
+                    GreetingBanner()
+                    PrimaryQuestCard()
+                    EnergyProgressCard()
+                    ActiveExchangeCard()
+                    QuickActionsRow()
+                }
+                .padding(.horizontal, AppMetric.screenHorizontal)
+                .padding(.top, 10)
+                .padding(.bottom, 24)
+            }
+        }
+        .safeAreaInset(edge: .bottom, spacing: 0) {
+            BottomTabBar()
+        }
+    }
+}
+
+#Preview("Home") {
+    HomeView()
+        .previewDevice("iPhone 15 Pro")
+}
