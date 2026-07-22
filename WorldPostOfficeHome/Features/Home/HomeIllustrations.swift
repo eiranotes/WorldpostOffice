0d1a671b80692ec9eb687557ec4dc29c85d3186d
eiranotes <eiradev000@gmail.com>
2026-07-22 14:47:30 +0900

    fix: restore generated home assets and align SwiftUI home
    Restore the generated transparent illustration assets in Assets.xcassets and update the SwiftUI home layout, typography, image sizing, spacing, and component composition to use those assets.

--- WorldPostOfficeHome/Features/Home/HomeIllustrations.swift
+++ WorldPostOfficeHome/Features/Home/HomeIllustrations.swift
@@ -2,57 +2,46 @@ import SwiftUI
 
 private struct AssetArt: View {
     let name: String
+    var contentMode: ContentMode = .fit
 
     var body: some View {
         Image(name, bundle: .main)
             .resizable()
+            .aspectRatio(contentMode: contentMode)
             .interpolation(.high)
-            .scaledToFit()
             .accessibilityHidden(true)
     }
 }
 
 struct CatAvatarArt: View {
-    var body: some View { AssetArt(name: "ProfileCatAvatar") }
+    var body: some View { AssetArt(name: "HomeProfileCatAvatar") }
 }
 
 struct BunnyAvatarArt: View {
-    var body: some View { AssetArt(name: "BunnyAvatar") }
+    var body: some View { AssetArt(name: "HomeBunnyAvatar") }
 }
 
-struct PlantPolaroidArt: View {
-    var body: some View { AssetArt(name: "PlantPolaroid") }
-}
-
-struct TokyoPostcardArt: View {
-    var body: some View { AssetArt(name: "TokyoPostcard") }
+struct HeroSceneArt: View {
+    var body: some View { AssetArt(name: "HomeHeroScene") }
 }
 
 struct HeartEnvelopeArt: View {
-    var body: some View { AssetArt(name: "HeartEnvelope") }
+    var body: some View { AssetArt(name: "HomeHeartEnvelope") }
 }
 
-struct MascotSceneArt: View {
-    var body: some View {
-        GeometryReader { proxy in
-            let size = proxy.size
+struct PlantPolaroidArt: View {
+    var body: some View { AssetArt(name: "HomePlantPolaroid") }
+}
 
-            ZStack(alignment: .bottomTrailing) {
-                AssetArt(name: "PostOfficeScene")
-                    .frame(width: size.width * 0.78)
-                    .offset(x: size.width * 0.04, y: size.height * 0.04)
+struct TokyoPostcardArt: View {
+    var body: some View { AssetArt(name: "HomeTokyoPostcard") }
+}
 
-                AssetArt(name: "MascotCat")
-                    .frame(width: size.width * 0.56)
-                    .offset(x: -size.width * 0.30, y: size.height * 0.02)
+struct EnergyStampArt: View {
+    var body: some View { AssetArt(name: "HomeEnergyStamp") }
+}
 
-                HeartEnvelopeArt()
-                    .frame(width: size.width * 0.18)
-                    .rotationEffect(.degrees(-8))
-                    .offset(x: -size.width * 0.60, y: -size.height * 0.42)
-            }
-            .frame(width: size.width, height: size.height)
-        }
-        .accessibilityHidden(true)
-    }
+struct QuickActionArt: View {
+    let name: String
+    var body: some View { AssetArt(name: name) }
 }
