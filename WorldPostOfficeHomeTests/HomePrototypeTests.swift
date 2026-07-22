616fc0e8615ae3ee223e9b9599fae9ec80a3b9a9
eiranotes <eiradev000@gmail.com>
2026-07-22 15:50:33 +0900

    test: align layout assertions with rebuilt home

--- WorldPostOfficeHomeTests/HomePrototypeTests.swift
+++ WorldPostOfficeHomeTests/HomePrototypeTests.swift
@@ -2,19 +2,14 @@ import XCTest
 @testable import WorldPostOfficeHome
 
 final class HomePrototypeTests: XCTestCase {
-    func testCoreLayoutMetricsRemainStable() {
-        XCTAssertEqual(AppMetric.screenHorizontal, 16)
+    func testCompactReferenceMetrics() {
+        XCTAssertEqual(AppMetric.horizontal, 14)
         XCTAssertEqual(AppMetric.cardRadius, 24)
-        XCTAssertEqual(AppMetric.tabBarHeight, 82)
+        XCTAssertEqual(AppMetric.tabHeight, 70)
     }
 
-    func testHomeContentUsesSupportedProgressRanges() {
-        let levelProgress = 720.0 / 1_000.0
-        let worldMailProgress = 60.0 / 100.0
-
-        XCTAssertGreaterThanOrEqual(levelProgress, 0)
-        XCTAssertLessThanOrEqual(levelProgress, 1)
-        XCTAssertGreaterThanOrEqual(worldMailProgress, 0)
-        XCTAssertLessThanOrEqual(worldMailProgress, 1)
+    func testProgressValuesAreValid() {
+        XCTAssertTrue((0...1).contains(720.0 / 1_000.0))
+        XCTAssertTrue((0...1).contains(60.0 / 100.0))
     }
 }
